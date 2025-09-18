133a064d0ee1570a9220e542511d93b8e6ccbea7
Jenifer Silva <jssilva423@gmail.com>
2025-09-18 17:22:24 -0300

    Add mocked tests

--- test/rest/controller/userControllerMocked.test.js
+++ test/rest/controller/userControllerMocked.test.js
@@ -4,25 +4,73 @@ const { expect } = require("chai");
 const app = require("../../../app.js");
 const userService = require("../../../service/userService.js");
 
-describe("User Controller", () => {
-  describe("POST /users/register", () => {
-    it("Deve retornar erro quando o usuário não for informado", async () => {
-      const userServiceMock = sinon.stub(userService, "registerUser");
-      userServiceMock.throws(new Error("Usuário e senha obrigatórios"));
+describe("User Controller - Mocked", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
 
-      const response = await request(app)
-        .post("/users/register")
-        .send({
-          username: `user${Math.random()}`,
-          password: "password",
-        });
-      console.log(response.body);
-      expect(response.status).to.equal(400);
-      expect(response.body.message).to.equal("Usuário e senha obrigatórios");
+  describe("POST /users/register", () => {
+    const businessErrorsTests = require("../fixture/requests/registerWithError.json");
+    businessErrorsTests.forEach((test) => {
+      it(`${test.testName}`, async () => {
+        const error = new Error(test.expectedMessage);
+        error.status = test.statusCode;
+        sinon.stub(userService, "registerUser").returns(error);
+
+        const response = await request(app)
+          .post("/users/register")
+          .send(test.createUser);
+        expect(response.status).to.equal(test.statusCode);
+        expect(response.body.message).to.equal(test.expectedMessage);
+      });
+    });
+
+    it("Deve criar usuário com sucesso", async () => {
+      const expectedResponse = require("../fixture/responses/registerSuccessful.json");
+      sinon.stub(userService, "registerUser").resolves({
+        message: expectedResponse.body.message,
+        user: { username: "user", password: "password" },
+      });
+
+      const response = await request(app).post("/users/register").send({
+        username: "username",
+        password: "password",
+      });
+
+      expect(response.status).to.equal(expectedResponse.statusCode);
+      expect(response.body).to.deep.equal(expectedResponse.body);
     });
   });
 
-  afterEach(() => {
-    sinon.restore();
+  describe("POST /users/login", () => {
+    const businessErrorsTests = require("../fixture/requests/loginWithError.json");
+
+    businessErrorsTests.forEach((test) => {
+      it(`${test.testName}`, async () => {
+        const error = new Error(test.expectedMessage);
+        error.status = test.statusCode;
+        sinon.stub(userService, "validateUser").rejects(error);
+
+        const response = await request(app)
+          .post("/users/login")
+          .send(test.loginUser);
+        expect(response.status).to.equal(test.statusCode);
+        expect(response.body.message).to.equal(test.expectedMessage);
+      });
+    });
+
+    it("Deve realizar o login com sucesso", async () => {
+      sinon.stub(userService, "validateUser").resolves({
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.mocktoken",
+      });
+
+      const response = await request(app).post("/users/login").send({
+        username: "username",
+        password: "password",
+      });
+
+      expect(response.status).to.equal(200);
+      expect(response.body.token).to.be.not.null;
+    });
   });
 });
